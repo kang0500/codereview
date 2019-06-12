dd7a81b77c233f48117a52f724a256f9d5896233
kang0500 <51424852+kang0500@users.noreply.github.com>
2019-06-12 15:49:26 +0900

    Update goods.cc

--- goods.cc
+++ goods.cc
@@ -1,10 +1,5 @@
 #include "./goods.h"
 
-// Set member variable of goods with given index and price values
-void Goods::SetGoodsInfo(int index, int price) {
-  goods_index_ = index;
-  goods_price_ = price;
-}
 
 // Return index information
 int Goods::GetIndex() {
