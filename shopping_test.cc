93b2062c2dca1a14a6eeaee9ede0345620077cb7
kang0500 <51424852+kang0500@users.noreply.github.com>
2019-06-12 15:53:05 +0900

    Update shopping_test.cc

--- shopping_test.cc
+++ shopping_test.cc
@@ -7,33 +7,24 @@
 
 
 TEST(shopping_list_test, goods_object_test) {
-  Goods testobject;
-
-  // Test 1 - Initalize
-  EXPECT_EQ(testobject.GetIndex(), 0);
-  ASSERT_EQ(testobject.GetPrice(), 0);
-
-  // Test 2 - Set Member Variable
-  testobject.SetGoodsInfo(1, 1000);
+  
+  // Test 2 - Set Member Variable 
+  Goods testobject(1, 1000);    
   EXPECT_EQ(testobject.GetIndex(), 1);
   ASSERT_EQ(testobject.GetPrice(), 1000);
 }
 
 TEST(shopping_list_test, shopping_object_test) {
   Shopping shopping_object;
-  Goods goods_test_object1;
-  Goods goods_test_object2;
-  Goods goods_test_object3;
-  Goods goods_test_object4;
-  Goods goods_test_object5;
-
+  
   // Make 5 Sample datas
-  goods_test_object1.SetGoodsInfo(3, 2500);
-  goods_test_object2.SetGoodsInfo(1, 10000);
-  goods_test_object3.SetGoodsInfo(5, 4500);
-  goods_test_object4.SetGoodsInfo(4, 49900);
-  goods_test_object5.SetGoodsInfo(2, 30000);
+  Goods goods_test_object1(3, 2500);
+  Goods goods_test_object2(1, 10000);
+  Goods goods_test_object3(5, 4500);
+  Goods goods_test_object4(4, 49900);
+  Goods goods_test_object5(2, 30000);
 
+ 
   shopping_object.PushList(goods_test_object1);
   shopping_object.PushList(goods_test_object2);
   shopping_object.PushList(goods_test_object3);
